597dadff28b5173473c858be17703be5665a4ac5
Manuel <Ramirez>
2020-05-05 13:04:58 -0400

    building front

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,24 @@
 import React, { Component } from "react";
+
 import logo from "./logo.svg";
 import "./App.css";
+import NavBar from "./components/Navbar";
+import Grid from "./components/Grid"
 
-class App extends Component {
-  render() {
+function App(){
+
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div>
+      <NavBar/>
+
+      <Grid/>
       </div>
+      
+        
+
+
     );
   }
-}
+
 
 export default App;
